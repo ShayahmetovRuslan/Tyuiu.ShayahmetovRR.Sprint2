7c2cd15b8e9727a50aa0af20ded157b4c1cd42e0
RaseWay <raseway@mail.ru>
2023-10-15 23:37:40 +0500

    Add project files.

---
+++ Tyuiu.ShayahmetovRR.Sprint2.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShayahmetovRR.Sprint2.Task0.V9.Lib;
+
+namespace Tyuiu.ShayahmetovRR.Sprint2.Task0.V9.Test
+{
+	[TestClass]
+	public class DataServiceTest
+	{
+		[TestMethod]
+		public async void ValidException()
+		{
+			DataService ds = new DataService();
+			int x = 1054;
+			int y = 375;
+			var res = ds.ValidException(x, y);
+			bool[] wait = new bool[6] { true, true, true, true, true, false }
+			CollectionAssert.AreEqual(wait, res);
+		}
+	}
+}
